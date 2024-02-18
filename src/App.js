440b63e6700b2e2a9d57870cfddc12b773f86cc3
KalaiBharathi-V <vkalaibharathi@gmail.com>
2024-02-18 17:05:05 +0530

    Product

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Product from "./Product";
+import Cart from "./Cart";
+import CartItems from "./CartItems";
+import Footer from "./Footer";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Product></Product>
+      <Cart></Cart>
+      <CartItems></CartItems>
+      <CartItems></CartItems>
+      <Footer></Footer>
     </div>
   );
 }
